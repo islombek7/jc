7a3fee6c6b1ef673517557d012195b9228e4c8dc
islombek <sirojr91@gmail.com>
2022-12-09 15:17:09 +0500

    homework

--- js/script.js
+++ js/script.js
@@ -1,37 +1,20 @@
-// let numberOfFilms = +prompt('Qancha film ko`rgansiz?');
-
-
-// let personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// }
-
-
-// let sawFilm = prompt('Oxirgi ko`rgan filimlaringizdan biri?');
-// let priceFilm = +prompt('Uni qancha baholagan bo`lardingiz?');
-
-
-// personalMovieDB.movies[sawFilm] = priceFilm;
 
 function pow (x,n){
-    x = 3
-    n =2
+    x = 3;
+    n =2;
     
-    console.log(x ** n)
+    console.log(x ** n);
     }
-    pow()
+    pow();
     
     
-    function sqrt(x,n){
-    x =  8
+function sqrt(x,n){
+    x =  8;
     n = 2
     
-    console.log(x ** 1/n)
+    console.log(x ** 1/n);
     }
-    sqrt()
+    sqrt();
 
 
 
