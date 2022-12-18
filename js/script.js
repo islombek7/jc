cd970809eaba04c88baf3f6cb65bb43becd180b0
islombek <sirojr91@gmail.com>
2022-12-18 10:36:54 +0500

    homework

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-//indexof, includes, startsWith, endsWith, slice, substring, substr
+// slice
 
-// function indexOf(str, searchString, pos = 0)
+let examplee = "Open web";
 
-
-// HomeWork-1 indexof
-
-function findIndex(str, Lorem){
-   let index = "OWA";
-   let inputs = 0;
-  for(let i = 0; i < str.length; i++) {
-    if(index[i]  !== Lorem ){
-      inputs++;
-    }else{
-      console.log(i);
-      break;
-    }
+function slice(str, start, end = str.length) {
+ let result = '';
+ for (; start < end; start++) {
+  result += str[start];
  }
+ return result;
 }
 
-findIndex(index, "W");
- console.log( findIndex());
+console.log(slice(examplee, 1, 6));
+
+// substring 
+
+let example1 = "Open web studycenter";
+
+function substring(str, start, end = str.length) {
+ let result = '';
+ for (; end < start; end++) {
+  result += str[end];
+ }
+ return result;
+}
+
+console.log(substring(example1, 8, 1));
+
+// substr 
+
+let example = 'Open Web' ;
+function myFunction( str, num, num1, ) {
+    let result1 = "";
+      for (i = num; i < num1; i++) {
+        result1 +=  str[i];
+      }
+      console.log(result1);
+    }
+    myFunction(example, 1, 4);
